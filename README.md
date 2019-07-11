58ee580f01e712919fd4359d2ba18f7f528eb30f
zcwfeng <zcwfeng@126.com>
2019-07-11 12:55:56 +0800

    init Tutorial 1

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # data_structure_tutorial
+
 数据结构演示和代码
+
 # 内容结构
 
 > tutorial1
+
+目录 ReaderFiles 是源代码
+
+目录 WorkshopApplets 可执行视图applet辅助理解数据结构
+
+用法：前提搭建好Java环境
+
+```
+cd WorkshopApplets
+appletviewer xxxx.html
+```
 
 > tutorial2
 
---
+++ tutorial1/ReaderFiles/Test/Test.java
@@ -0,0 +1,59 @@
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Dictionary;
+import java.util.Deque;
+import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.SortedMap;
+import java.util.Enumeration;
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.NavigableSet;
+import java.util.NavigableMap;
+import java.util.WeakHashMap;
+import java.util.Vector;
+// import java.util.concurrent.atomic;
+// import java.util.concurrent.locks;
+// import java.util.zip.*;
+// import java.util.stream.*;
+// import java.util.spi.*;
+// import java.util.regex.*;
+// import java.util.prefs.*;
+// import java.util.logging.*;
+// import java.util.jar.*;
+// import java.util.function.*;
+// import java.util.ListIterator;
+// import java.util.Formattable;
+// import java.util.Iterator;
+// import java.util.Comparator;
+// import java.util.UUID;
+// import java.util.RandomAccess;
+// import java.util.PrimitiveIterator;
+// import java.util.Observer;
+// import java.util.Spliterator;
+
+
+
+
+
+class Test{
+    public static void main(String[] args) {
+
+    }
+}
